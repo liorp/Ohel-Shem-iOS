8de1299750bc20b473d6c97e60441340daab4fdf
harrym17 <harrym17@gmail.com>
2015-03-01 11:58:22 +0200

    More legible widget and summery

--- Ohel Shem/MainHomePage.swift
+++ Ohel Shem/MainHomePage.swift
@@ -90,14 +90,14 @@ class MainHomePage: UIViewController {
                 //If we don't, we show the corresponding hour
                 var theHourWith = hours[i - 1].componentsSeparatedByString(" ")
                 theHourWith.insert("עם", atIndex: 1)
-                var display = join(" ", theHourWith) as String
-                changes.appendAttributedString(NSAttributedString(string: "•בשעה ה\(String(numberToHebrewNumbers[i]!)) יש \(display)\n", attributes: attrBody))
+                let display = join(" ", theHourWith) as String
+                changes.appendAttributedString(NSAttributedString(string: "◉בשעה ה\(String(numberToHebrewNumbers[i]!)) יש \(display)\n", attributes: attrBody))
             } else {
                 if hours[i-1] == "שעה חופשית!" {
-                    let attrEmptyHour = NSAttributedString(string: "•בשעה ה\(String(numberToHebrewNumbers[i]!)) יש \(hours[i-1])\n", attributes: attrBody)
+                    let attrEmptyHour = NSAttributedString(string: "◉בשעה ה\(String(numberToHebrewNumbers[i]!)) יש \(hours[i-1])\n", attributes: attrBody)
                     changes.appendAttributedString(attrEmptyHour)
                 } else {
-                    let attrEmptyHour = NSAttributedString(string: "•בשעה ה\(String(numberToHebrewNumbers[i]!)) \(hours[i-1])\n", attributes: attrBody)
+                    let attrEmptyHour = NSAttributedString(string: "◉בשעה ה\(String(numberToHebrewNumbers[i]!)) \(hours[i-1])\n", attributes: attrBody)
                     changes.appendAttributedString(attrEmptyHour)
                 }
             }
@@ -106,7 +106,7 @@ class MainHomePage: UIViewController {
             if i - 1 < changesString.count {
                 if (changesString[i - 1] != "-"){
                     //If we have a change, we show it
-                    let attrChange = NSAttributedString(string: "•בשעה ה\(String(numberToHebrewNumbers[i]!)): " + changesString[i - 1] + "\n", attributes: [NSFontAttributeName : fontSubHead! , NSForegroundColorAttributeName: UIColor.redColor()])
+                    let attrChange = NSAttributedString(string: "◉בשעה ה\(String(numberToHebrewNumbers[i]!)): " + changesString[i - 1] + "\n", attributes: [NSFontAttributeName : fontSubHead! , NSForegroundColorAttributeName: UIColor.redColor()])
                     changes.appendAttributedString(attrChange)
                 } else {
                     AppendRegularHour(i)
